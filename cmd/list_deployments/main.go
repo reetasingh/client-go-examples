7c034df5f12cf10685d019f63fc9abb450f7dee7
reetasingh <reetasingh@salesforce.com>
2021-04-03 16:37:39 -0700

    adding for deployments

---
+++ cmd/list_deployments/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"path/filepath"
+)
+
+func main() {
+
+	// get kubeconfig file
+	var kubeconfig *string
+	homeDir := homedir.HomeDir()
+	fmt.Println(homeDir)
+
+	fmt.Println(filepath.Join(homeDir, ".kube", "config"))
+
+	kubeconfig = flag.String("kubeconfig", filepath.Join(homeDir, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	fmt.Println(kubeconfig)
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// List Secret
+	secrets, err := clientset.AppsV1().Deployments("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for i := 1; i < len(secrets.Items); i++ {
+		secret := secrets.Items[i]
+		fmt.Printf("Secret name %s\n", secret.ObjectMeta.Name)
+		fmt.Printf("Namespace %s\n", secret.ObjectMeta.Namespace)
+		fmt.Printf("ResourceVersion %s\n", secret.ResourceVersion)
+		fmt.Printf("Data %s\n", secret.Data)
+		fmt.Printf("Labels %s\n", secret.Labels)
+		fmt.Printf("Annotations %s\n", secret.Annotations)
+		fmt.Printf("===========")
+	}
+}
