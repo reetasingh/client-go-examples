93293edd6c6c24ffb765aef58e5303cb3097388a
reetasingh <reetasingh@salesforce.com>
2021-04-03 17:28:38 -0700

    fix daemonsets

--- cmd/list_daemonsets/main.go
+++ cmd/list_daemonsets/main.go
@@ -16,12 +16,8 @@ func main() {
 	// get kubeconfig file
 	var kubeconfig *string
 	homeDir := homedir.HomeDir()
-	fmt.Println(homeDir)
-
-	fmt.Println(filepath.Join(homeDir, ".kube", "config"))
 
 	kubeconfig = flag.String("kubeconfig", filepath.Join(homeDir, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	fmt.Println(kubeconfig)
 
 	// use the current context in kubeconfig
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
@@ -36,15 +32,17 @@ func main() {
 	}
 
 	// List namespaces
-	daemonsets, err := clientset.AppsV1().DaemonSets().List(context.TODO(), metav1.ListOptions{})
+	daemonsets, err := clientset.AppsV1().DaemonSets("").List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		panic(err.Error())
 	}
 
 	for i := 0; i < len(daemonsets.Items); i++ {
 		daemonset := daemonsets.Items[i]
-		fmt.Printf("Namespace name %s\n", daemonset.ObjectMeta.Name)
-		fmt.Printf("Status %s\n", daemonset.Status)
+		fmt.Printf("DaemonSet name %s\n", daemonset.Name)
+		fmt.Printf("Namespace %s\n", daemonset.ObjectMeta.Namespace)
+		fmt.Printf("Selector %s\n", daemonset.Spec.Selector)
+		fmt.Printf("Pod Spec %s\n", daemonset.Spec.Template.Spec)
 		fmt.Printf("ResourceVersion %s\n", daemonset.ResourceVersion)
 		fmt.Printf("Labels %s\n", daemonset.Labels)
 		fmt.Printf("Annotations %s\n", daemonset.Annotations)
