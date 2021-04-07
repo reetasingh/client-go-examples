2ef6d187fc3a4096c97f4e0238d70593a6e0b4ab
reetasingh <reetasingh@salesforce.com>
2021-04-06 23:35:05 -0700

    add code for update

---
+++ cmd/update_pod/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"path/filepath"
+)
+
+func main() {
+
+	// get kubeconfig file
+	var kubeconfig *string
+	homeDir := homedir.HomeDir()
+
+	kubeconfig = flag.String("kubeconfig", filepath.Join(homeDir, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Get pod with name abc in namespace default
+	pod, err := clientset.CoreV1().Pods("default").Get(context.TODO(), "abc", metav1.GetOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Printf("Pod name %s\n", pod.ObjectMeta.Name)
+	fmt.Printf("Namespace %s\n", pod.ObjectMeta.Namespace)
+	fmt.Printf("ResourceVersion %s\n", pod.ResourceVersion)
+
+	containers := pod.Spec.Containers
+	fmt.Printf("Number of containers = %d\n", len(containers))
+
+	fmt.Println("Containers")
+	for j := range containers {
+		fmt.Printf("    Container Name %s\n", containers[j].Name)
+		fmt.Printf("    Container Image %s\n", containers[j].Image)
+
+	}
+
+	fmt.Printf("Labels %s\n", pod.Labels)
+
+	fmt.Printf("Annotations %s\n", pod.Annotations)
+	fmt.Println("==================")
+
+	// Get pod with name abc in namespace default
+	pod1, err := clientset.CoreV1().Pods("default").Update(context.TODO(), pod, metav1.UpdateOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Printf("Pod name %s\n", pod1.ObjectMeta.Name)
+	fmt.Printf("Namespace %s\n", pod1.ObjectMeta.Namespace)
+	fmt.Printf("ResourceVersion %s\n", pod1.ResourceVersion)
+
+}
